d6f02be0683be6011b8f7b38840cefecf222a06d
SeungKyuHan <lollipop0939@gmail.com>
2026-04-30 00:11:33 +0900

    48 upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+window.GOLDENBELL_FIREBASE = {
+  enabled: true,
+  config: {
+    apiKey: "AIzaSyBptCyg4fCSUJRXWdKgxGgGQQQREnpjCBM",
+    authDomain: "golden-bell-physics.firebaseapp.com",
+    projectId: "golden-bell-physics",
+    storageBucket: "golden-bell-physics.firebasestorage.app",
+    messagingSenderId: "378382672283",
+    appId: "1:378382672283:web:a550fb23a48dbe66dcc13e",
+  }
+};
